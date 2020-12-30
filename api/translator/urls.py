5bc91153c868f996202aed83bb7c2e48b6970e35
Nicoconte <nicoconte1999@gmail.com>
2020-12-29 21:59:33 -0300

    Update: Some methods were removed due heroku´s filesystem. We only can post data and receive a file object (blob) as a response

--- api/translator/urls.py
+++ api/translator/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
 
-from .views import TextToSpeechBlobView, TextToSpeechUrlView
+from .views import TextToSpeechBlobView
 
 urlpatterns = [
     path('text-to-speech/blob/', TextToSpeechBlobView.as_view()), #post
-    path('text-to-speech/blob/<str:name>/', TextToSpeechBlobView.as_view()),
-    path('text-to-speech/blob/?<str:name>/', TextToSpeechBlobView.as_view()), #get
-    
-    path('text-to-speech/url/', TextToSpeechUrlView.as_view()), #post 
-    path('text-to-speech/url/<str:name>/', TextToSpeechUrlView.as_view()),
-    path('text-to-speech/url/?<str:name>/', TextToSpeechUrlView.as_view()), #get
-
 ] 
--- api/translator/views.py
+++ api/translator/views.py
@@ -1,6 +1,5 @@
-from gtts_api.settings import MEDIA_ROOT
 from django.core.files.storage import FileSystemStorage
-from django.http import JsonResponse, FileResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse
 from django.conf import settings
 
 from rest_framework.views import APIView
@@ -11,6 +10,10 @@ from .utils import Utils
 import json
 
 
+"""
+We only need post method due that heroku doesnt persistance the uploaded files. Heroku´s filesystem is ephemeral 
+For more information: https://help.heroku.com/K1PPS2WM/why-are-my-file-uploads-missing-deleted 
+"""
 class TextToSpeechBlobView(APIView):
     utils = Utils()
 
@@ -26,91 +29,9 @@ class TextToSpeechBlobView(APIView):
 
             response = HttpResponse(file_object, content_type="audio/mpeg")
             response['Content-Disposition'] = 'attachment; filename="%s"' % file_name
-            #response = FileResponse(file_object)
-
             return response
 
         else:
             return JsonResponse({"error" : "An error occurred during convertion"}, status=status.HTTP_400_BAD_REQUEST)   
-
-
-    def get(self, request):
-        file_name = request.query_params['name']
-
-        if self.storage.exists(file_name):
-            file_object = self.storage.open(file_name, mode="rb")
-
-            response = HttpResponse(file_object, content_type="audio/mpeg")
-            response['Content-Disposition'] = 'attachment; filename=%s' % file_name
-
-            return response
-
-        else:
-            return JsonResponse({"error" : "Resource doesnt exist"}, status=status.HTTP_404_NOT_FOUND)
-
-    def delete(self, request, name):
-        file_name = name #request.query_params['name']    
-        print(file_name)
-
-        if self.storage.exists(file_name):
-            self.storage.delete(file_name)
-
-            return JsonResponse({"msg" : "The resource was removed from storage"}, status=status.HTTP_200_OK)
-        
-        else:
-            return JsonResponse({"error" : "Resource doesnt exist"}, status=status.HTTP_404_NOT_FOUND)            
-
-
-
-class TextToSpeechUrlView(APIView):
-    utils = Utils()
-
-    storage = FileSystemStorage(location=settings.MEDIA_URL)
-
-    def post(self, request):
-        print(request.body)
-        body_decoded = json.loads(request.body)
-        print(body_decoded)
-
-        file_created, file_name = self.utils.translate(body_decoded, settings.MEDIA_ROOT)
-        
-        if file_created:            
-            url = self.storage.url(file_name)
-
-            return JsonResponse({"audio_url" : url, "name" : file_name}, status=status.HTTP_200_OK)
-
-        else:
-            return JsonResponse({"error" : "An error occurred during convertion"}, status=status.HTTP_400_BAD_REQUEST)
-
-
-    def get(self, request):
-        file_name = request.query_params['name']
-
-        self.storage = FileSystemStorage(location=settings.MEDIA_ROOT)
-
-        if self.storage.exists(file_name):
-
-            self.storage = FileSystemStorage(location=settings.MEDIA_URL)
-            url = self.storage.url(file_name)
-
-            return JsonResponse({"audio_url" : url}, status=status.HTTP_200_OK)
-
-        else:
-            return JsonResponse({"error" : "Resource doesnt exist"}, status=status.HTTP_404_NOT_FOUND)
-
-    
-    def delete(self, request, name):
-        file_name = name #request.query_params['name']    
-        print(file_name)
-
-        self.storage = FileSystemStorage(location=settings.MEDIA_ROOT)
-
-        if self.storage.exists(file_name):
-            self.storage.delete(file_name)
-
-            return JsonResponse({"msg" : "The resource was removed from storage"}, status=status.HTTP_200_OK)
-        
-        else:
-            return JsonResponse({"error" : "Resource doesnt exist"}, status=status.HTTP_404_NOT_FOUND)            
 
 
